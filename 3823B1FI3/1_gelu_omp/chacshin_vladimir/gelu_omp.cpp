5e9a17b3fc9b495ee590d820cd986e8c0db98ba7
kodt2 <kodt2005@yandex.ru>
2026-02-27 23:13:41 +0300

    upgrade spped from 172ms to 160ms

--- 3823B1FI3/1_gelu_omp/chacshin_vladimir/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/chacshin_vladimir/gelu_omp.cpp
@@ -6,6 +6,11 @@
 #include<chrono>
 #include<iostream>
 #include "gelu_omp.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
 
 
 inline __m256 exp256_ps_small(__m256 x) {
@@ -55,10 +60,52 @@ inline __m256 exp256_ps_small(__m256 x) {
     return _mm256_mul_ps(poly, pow2n);
 }
 
+inline __m256 _mm256_qexp_estrin_ps(__m256 _x)
+{
+    const __m256 _one = _mm256_set1_ps(1.0f);
+    const __m256 _ln2r = _mm256_set1_ps(1.44269504088896341f);
+
+    const __m256 _ln2a = _mm256_set1_ps(-0.693145751953125f);
+    const __m256 _ln2b = _mm256_set1_ps(-1.428606820309417e-6f);
+
+    const __m256 _c7 = _mm256_set1_ps(1.9875691500E-4f);
+    const __m256 _c6 = _mm256_set1_ps(1.3981999507E-3f);
+    const __m256 _c5 = _mm256_set1_ps(8.3334519073E-3f);
+    const __m256 _c4 = _mm256_set1_ps(4.1665795894E-2f);
+    const __m256 _c3 = _mm256_set1_ps(1.6666665459E-1f);
+    const __m256 _c2 = _mm256_set1_ps(5.0000001201E-1f);
+
+    __m256 _n = _mm256_round_ps(
+        _mm256_mul_ps(_x, _ln2r),
+        _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+
+    __m256 _g = _mm256_fmadd_ps(_n, _ln2b,
+        _mm256_fmadd_ps(_n, _ln2a, _x));
+
+    __m256 _g2 = _mm256_mul_ps(_g, _g);
+    __m256 _g4 = _mm256_mul_ps(_g2, _g2);
+
+    __m256 _p1 = _mm256_fmadd_ps(_c7, _g, _c6);
+    __m256 _p2 = _mm256_fmadd_ps(_c5, _g, _c4);
+    __m256 _p3 = _mm256_fmadd_ps(_p1, _g2, _p2);
+
+    __m256 _p4 = _mm256_fmadd_ps(_c3, _g, _c2);
+    __m256 _p5 = _mm256_fmadd_ps(_p4, _g2, _mm256_add_ps(_g, _one));
+
+    __m256 _y = _mm256_fmadd_ps(_p3, _g4, _p5);
+
+    __m256i _n_int = _mm256_cvtps_epi32(_n);
+    __m256i _exp_offset = _mm256_add_epi32(_n_int, _mm256_set1_epi32(127));
+    __m256i _exp_shifted = _mm256_slli_epi32(_exp_offset, 23);
+    __m256 _two_pow_n = _mm256_castsi256_ps(_exp_shifted);
+
+    return _mm256_mul_ps(_y, _two_pow_n);
+}
+
 std::vector<float> GeluOMP(const std::vector<float>& input) {
     size_t n = input.size();
     std::vector<float> output(n);
-    int block_size = 16800;
+    int block_size = 12400;
 
 
     const float alpha = 0.79788456f;
@@ -73,6 +120,7 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
         size_t i;
 
         for (i = b; i < end - 7; i += 8) {
+            _mm_prefetch((const char*)(input.data() + i + 128), _MM_HINT_T0);
             __m256 x = _mm256_loadu_ps(input.data() + i);
 
             __m256 mask_high = _mm256_cmp_ps(x, thresh_high, _CMP_GT_OS);   // x > 10
@@ -86,7 +134,7 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
 
             __m256 two_u = _mm256_add_ps(u, u);
 
-            __m256 exp2u = exp256_ps_small(two_u);
+            __m256 exp2u = _mm256_qexp_estrin_ps(two_u);
 
 
             __m256 tanh_u = _mm256_sub_ps(_mm256_set1_ps(1.0f),
@@ -99,7 +147,7 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
             res = _mm256_blendv_ps(res, x, mask_high); // если x>10 ставим x
             res = _mm256_blendv_ps(res, zero_vec, mask_low); // если x<-9 ставим 0
 
-            _mm256_storeu_ps(output.data() + i, res);
+            _mm256_stream_ps(output.data() + i, res);
         }
 #pragma omp simd
         for (int j = i; j < end; ++j) {
@@ -110,11 +158,11 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
             output[j] = 0.5f * x * (1.0f + tanh_u);
         }
     }
-
+    _mm_sfence();
     return output;
 }
 
-/*double test_performance(const std::vector<float>& input, size_t repeats = 4, int block_size = 16800) {
+double test_performance(const std::vector<float>& input, size_t repeats = 4, int block_size = 16800) {
     // Warming-up
     GeluOMP(input);
     // Performance Measuring
@@ -137,57 +185,17 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
     return avg;
 }
 
-int main() {
-    FILE* fgold = fopen("golden_100_100.bin", "rb");
-    if (!fgold) {
-        std::cerr << "Cannot open golden file\n";
-        return 1;
-    }
-
-    std::vector<float> input;
-    std::vector<float> golden;
-
-    // читаем из stdin и golden одновременно
-    float x, y;
-    while (fread(&x, sizeof(float), 1, stdin) == 1 &&
-        fread(&y, sizeof(float), 1, fgold) == 1) {
-        input.push_back(x);
-        golden.push_back(y);
-    }
-
-    fclose(fgold);
-
-    std::vector<float> outp;
-    outp = GeluOMP(input);
-    for (int i=0;i< input.size();i++){
-        if (std::abs(outp[i] - golden[i]) > 1e-7) {
-            std::cout<<i
-                << "inp:" << input[i]
-                << " outp:" << outp[i]
-                << " exp:" << golden[i]
-                << " diff:" << (outp[i] - golden[i])
-                << "False"
-                << "\n";
-        }
-        else {
-            std::cout << i
-                << "inp:" << input[i]
-                << " outp:" << outp[i]
-                << " exp:" << golden[i]
-                << " diff:" << (outp[i] - golden[i])
-                << "\n";
-        }
-    }
+/*int main() {
     omp_set_num_threads(4);
     using clock = std::chrono::high_resolution_clock;
 
-    std::vector<float> input2(134217728); // пример: 16M элементов
+    std::vector<float> input2(134217728);
     std::mt19937 rng(42);
     std::uniform_real_distribution<float> dist(-100,100);
 
     for (auto& x : input2) x = dist(rng);
 
-    test_performance(input2, 20);
+    test_performance(input2, 100);
 
     return 0;
 }*/
